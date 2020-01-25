ee13c1d2b70e574cdd751cc47619d8f5dafb0b14
Yassine El Ouni <yassine.ouni001@gmail.com>
2020-01-25 11:56:43 +0100

    first commit

---
+++ src/contacts/services/contacts.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Contact } from '../models/contact.entity';
+import { UpdateResult, DeleteResult } from  'typeorm';
+
+@Injectable()
+export class ContactsService {
+
+    /**
+     *
+     */
+    constructor(@InjectRepository(Contact)
+    private contactRepository: Repository<Contact>) {
+    }
+    /**
+     *
+     */
+
+    async  findAll(): Promise<Contact[]> {
+        return await this.contactRepository.find();
+    }
+
+    async  create(contact: Contact): Promise<Contact> {
+        return await this.contactRepository.save(contact);
+    }
+
+    async update(contact: Contact): Promise<UpdateResult> {
+        return await this.contactRepository.update(contact.id, contact);
+    }
+
+    async delete(id): Promise<DeleteResult> {
+        return await this.contactRepository.delete(id);
+    }
+}
